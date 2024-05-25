b919c07c98d2063093bfbc3e916ca7d07bd8a920
Balaji <160224234+BALAJI-PRO-001@users.noreply.github.com>
2024-05-25 19:06:58 +0530

    Delete Routing directory

--- Routing/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = require("./routes/response.route");
-const app = express();
-
-app.use("/app", router);
-
-app.listen(3000, () => console.log("Server is running ...."));
-
